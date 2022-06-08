da8c42aa96c3cade987e07867a13539688668a02
Vanessa Wagner <vmwagner@gmail.com>
2022-06-07 19:36:19 -0500

    commit tree 183c4d278033c68d2e136a1eef6377ade1cd5b83

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Add your code here
+function submitData (userName, userEmail) {
+    const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json"
+        },
+        body: JSON.stringify({
+            "name": userName,
+            "email": userEmail
+        })
+    };
+    return fetch ('http://localhost:3000/users', configurationObject)
+
+    .then(res => res.json()) 
+
+    .then(object => document.body.innerHTML = object.id)
+
+    .catch(error => document.body.innerHTML = error.message)
+
+}
+
+submitData("Name", "name@name.com")
